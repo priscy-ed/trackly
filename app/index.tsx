e28325d942748c6d83a6f192d5030a02a0bfcf27
priscyd <priscilladunyoh99@gmail.com>
2026-02-24 16:11:00 +0000

    hangout font implemented

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
 import { Stack } from 'expo-router';
 
 import { View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { fonts } from '@/core/theme/font';
 
 export default function Home() {
+  const [fontsLoaded] = useFonts({
+    Hangout: require('../assets/fonts/quarantype-hangout.ttf'),
+  });
+
+  if (!fontsLoaded) return null;
   return (
     <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
+      <Stack.Screen options={{ title: 'Trackly', headerTitleStyle: { fontFamily: fonts.tertiary } }} />
     </View>
   );
 }
