b3adb80a3862c9a57844b46f26d1d24747229826
priscyd <priscilladunyoh99@gmail.com>
2026-03-04 23:38:20 +0000

    small updates

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,26 +9,36 @@ const TabLayout = () => {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: COLORS.blue[400],
+        tabBarStyle: {
+          backgroundColor: COLORS.red[500],
+          position: 'absolute',
+          borderTopWidth: 0,
+          elevation: 4,
+          width: '90%',
+          alignSelf: 'center',
+          bottom: 10,
+          borderRadius: 50,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 5,
+        },
       }}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Home",
-            tabBarIcon: ({ color }) => (
-              <TabBarIcon name="home-outline" color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            title: "Profile",
-            tabBarIcon: ({ color }) => (
-              <TabBarIcon name="person-outline" color={color} />
-            ),
-          }}
-        />
-      </Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home-outline" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <TabBarIcon name="person-outline" color={color} />,
+        }}
+      />
+    </Tabs>
   );
 };
 
